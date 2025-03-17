4f7c3d5fec148eb2436315ba0c8963ca99c70650
Mohamed Ammar <mohamed.ammar@asuresoftware.com>
2025-03-16 22:56:46 -0500

    feat: Add Header and TotalBalance components

---
+++ src/components/TotalBalance.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4>Your Balance</h4>
+    <h1 id="balance">${{ balance }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+
+// const balance = computed(() => props.totalIncome - props.totalExpense)
+
+const props = defineProps({
+  transactions: Array as PropType<Transaction[]>,
+})
+</script>
